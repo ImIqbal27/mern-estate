cb659e2b06c5c6750614eee2e86a74ee6f62ec64
Iqbal <iamhasan9501@gmail.com>
2023-10-06 23:54:07 +0600

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -14,6 +15,7 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server  is running to the port 3000 !!!.");
 });
@@ -22,3 +24,5 @@ app.listen(3000, () => {
 //   res.json({ message: "Hi world33" });
 // });
 app.use("/api/user", userRouter);
+// ///////////// for signup //////////////////////////////////////////
+app.use("/api/auth", authRouter);
